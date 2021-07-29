f514a4ddacef77d5c56463c68c75d61cdd18627d
surajborage <borage.suraj113@gmail.com>
2021-07-29 18:19:17 +0530

    modified:dilyempwage

--- empwageAttendance.sh
+++ empwageAttendance.sh
@@ -1,11 +1,13 @@
+#! /bin/bash -x
 #!/bin/bash -x
 
 isPresent=1;
 randomCheck=$((RANDOM%2));
-
+WagePerHr=20
 if [ $isPresent -eq $randomCheck ];
 then
-        echo "Employee is Present";
+        empHrs=8;
 else
-        echo "Employee is Absent";
+        empHrs=0;
 fi
+salary=$(($WagePerHr * $empHrs))
