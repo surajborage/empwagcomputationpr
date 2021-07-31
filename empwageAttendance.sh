659faa6957bc1a6ab1b743eca4e0db4ad44df76f
surajborage <borage.suraj113@gmail.com>
2021-07-31 11:44:25 +0530

    modified:switchCaseStatement

--- empwageAttendance.sh
+++ empwageAttendance.sh
@@ -1,16 +1,34 @@
-#!/bin/bash -x
-
+#! /bin/bash -x
+declare -a dailyWage
 isPresentFull=1;
 isPresentHalf=2;
-randomCheck=$((RANDOM%3));
 WagePerHr=20
-if [ $isPresentFull -eq $randomCheck ];
-then
-        empHrs=8;
-elif [ $isPresentHalf -eq $randomCheck ];
-then
-	empHrs=4;
-else
-        empHrs=0;
-fi
-salary=$(($WagePerHr * $empHrs))
+MaxWorkDays=20
+MaxWorkHr=100
+TotalWorkhr=0
+day=0
+function getWorkHr() {
+		local randomCheck=$1
+		local emphrs=0
+	case $randomCheck in
+		$isPresentFull)
+		empHrs=8 ;;
+		$isPresentHalf)
+		empHrs=4 ;;
+		*)
+        	empHrs=0 ;;
+	esac
+	echo $empHrs
+}
+while [ $day -lt $MaxWorkDays ] && [ $TotalWorkhr -lt $MaxWorkHr ]
+do
+        randomCheck=$((RANDOM%3));
+        ((day++))
+	empHrs="$(getWorkHr $randomCheck)"
+	TotalWorkHr=$((TotalWorkHr + empHrs))
+	salary=$((WagePerHr * empHrs))
+done
+TotalSalary=$((WagePerHr * TotalWorkHr))
+echo dailyWages:${!dailywage[*]}
+echo day:${day[*]}
+© 2021 GitHub, Inc.
